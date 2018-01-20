e74c9816155ac7f9315cfbe1627ab8cf8ba2fa26
Jay MOULIN <jaymoulin@gmail.com>
2018-01-20 00:00:15 +0000

    Update to 1.1.1.0

---
+++ js/content.js
@@ -0,0 +1,49 @@
+if (chrome && chrome.runtime) {
+    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+        let discolumns = hackDisco();
+        if (discolumns) {
+            sendResponse({"type": 'disco', "tabId": message.tabId, "url": window.location.hostname, "struct": discolumns});
+            return;
+        }
+        let struct = hackJSvars();
+        if (struct) {
+            sendResponse({"url": struct.id, "tabId": message.tabId, "struct": struct, "type":'album'});
+            return;
+        }
+    });
+}
+
+function hackJSvars() {
+    let pagedata = document.getElementById("pagedata");
+    if (typeof(pagedata) === 'undefined' || pagedata === null) {
+        return null;
+    }
+
+    let script = document.createElement('script');
+    let scriptContent = "document.getElementById(\"pagedata\").title = JSON.stringify(TralbumData);";
+    script.id = 'tmpScript';
+    script.appendChild(document.createTextNode(scriptContent));
+    (document.body || document.head || document.documentElement).appendChild(script);
+
+    let el = (JSON.parse(pagedata.title));
+    el.albumart = document.getElementById("tralbumArt").getElementsByTagName('img')[0].src;
+    return el;
+}
+
+function hackDisco() {
+    let discolumns = document.getElementsByClassName('leftMiddleColumns');
+    if (typeof(discolumns) === 'undefined' || discolumns === null) {
+        return null;
+    }
+    if (discolumns.length) {
+        let discol = discolumns[0].getElementsByTagName('ol');
+        if (discol.length) {
+            let albumart = document.getElementsByClassName("popupImage");
+            return {
+                albums:eval(discol[0].getAttribute('data-initial-values')),
+                albumart:albumart.length ? albumart[0].getAttribute('href') : null
+            };
+        }
+    }
+    return null;
+}
---
+++ js/settings.js
@@ -0,0 +1,247 @@
+class Settings {
+    constructor(options) {
+        if (options instanceof Options) {
+            this.options = options
+        }
+        this
+            .renderPage()
+            .bindMenu()
+            .createNotifications()
+            .bindNotifications()
+            .bindOptions()
+            .toggleNotifications()
+            .toggleOptions()
+            .createDownloads()
+            .bindDownloads()
+    }
+
+    renderPage() {
+        if (window.location.hash == '#settings') {
+            document.getElementById('settings').style.display = 'block'
+            document.getElementById('downloads').style.display = 'none'
+            document.getElementById('menu_settings').className = 'active'
+            document.getElementById('menu_downloads').className = ''
+        } else {
+            document.getElementById('settings').style.display = 'none'
+            document.getElementById('downloads').style.display = 'block'
+            document.getElementById('menu_settings').className = ''
+            document.getElementById('menu_downloads').className = 'active'
+        }
+        return this
+    }
+
+    bindMenu() {
+        window.onpopstate = this.renderPage
+        return this
+    }
+
+    createNotifications() {
+        let htmlNotif = ''
+        let htmlOptions = ''
+        for (let index in this.options.notifMapping) {
+            let mapping = this.options.notifMapping[index]
+            htmlNotif += `<li class="list-group-item">
+                    ${mapping.display}
+                    <div class="material-switch pull-right">
+                        <input id="${mapping.name}" type="checkbox" title="${mapping.title}"/>
+                        <label for="${mapping.name}" class="label-primary" title="${mapping.title}"></label>
+                    </div>
+                </li>`
+        }
+        for (let index in this.options.optionsMapping) {
+            let mapping = this.options.optionsMapping[index]
+            htmlOptions += `<li class="list-group-item">
+                    ${mapping.display}
+                    <div class="material-switch pull-right">
+                        <input id="${mapping.name}" type="checkbox" title="${mapping.title}"/>
+                        <label for="${mapping.name}" class="label-primary" title="${mapping.title}"></label>
+                    </div>
+                </li>`
+        }
+
+        document.getElementById('notifications_list').innerHTML = htmlNotif
+        document.getElementById('options_list').innerHTML = htmlOptions
+        return this
+    }
+
+
+    bindNotifications() {
+        for (let index in this.options.notifMapping) {
+            let mapping = this.options.notifMapping[index]
+            document.getElementById(mapping.name).addEventListener('change', evt => this.options.setNotification(index, evt.target.checked))
+        }
+        return this
+    }
+
+    toggleNotifications() {
+        for (let index in this.options.notifMapping) {
+            let mapping = this.options.notifMapping[index]
+            if (this.options.getNotification(index)) {
+                document.getElementById(mapping.name).checked = 'checked'
+            }
+        }
+        return this;
+    }
+
+    bindOptions() {
+        for (let index in this.options.optionsMapping) {
+            let mapping = this.options.optionsMapping[index]
+            document.getElementById(mapping.name).addEventListener('change', evt => this.options.setOption(index, evt.target.checked))
+        }
+        return this
+    }
+
+    toggleOptions() {
+        for (let index in this.options.optionsMapping) {
+            let mapping = this.options.optionsMapping[index]
+            if (this.options.getOption(index)) {
+                document.getElementById(mapping.name).checked = 'checked'
+            }
+        }
+        return this;
+    }
+
+    createDownloads() {
+        let html = ''
+        let hasOneError = false
+        let hasOneWaiting = false
+        let hasOneNotStarted = false
+        for (let url in chrome.extension.getBackgroundPage().objList) {
+            let component = chrome.extension.getBackgroundPage().objList[url]
+            hasOneNotStarted = hasOneNotStarted || !component.started
+            let tracks = ''
+            let tracksHasError = false
+            let tracksHasWaiting = false
+            component.tracks.forEach(function (info, trackId) {
+                let status = ''
+                if (info.success === false) {
+                    status = `<a class="label label-danger jsRetryTrack" data-url="${url}" data-trackid="${trackId}">Failed / Retry</a>`
+                    tracksHasError = true
+                    hasOneError = true
+                } else if (info.success === true) {
+                    status = '<span class="label label-success">Success</span>'
+                } else {
+                    status = '<span class="label label-warning">Waiting</span>'
+                    tracksHasWaiting = true
+                    hasOneWaiting = true
+                }
+                tracks += `<li class="col-xs-12">
+                                <div class="col-xs-10">${info.track}</div>
+                                <div class="col-xs-2">
+                                    ${status}
+                                </div>
+                            </li>`
+            })
+            let progress = chrome.extension.getBackgroundPage().getProgress(url)
+            let progressBar = progress === 100
+                ? ''
+                : `<div class="${tracksHasError && !tracksHasWaiting ? 'col-xs-8 col-md-9' : 'col-xs-12'}">
+                    <div class="progress">
+                        <div class="progress-bar ${tracksHasError ? (tracksHasWaiting ? 'progress-bar-warning' : 'progress-bar-danger') : 'progress-bar-striped'}" role="progressbar" aria-valuenow="${progress}" aria-valuemin="0" aria-valuemax="100" style="width: ${progress}%"><span class="sr-only">${progress}% Complete</span></div>
+                    </div>
+                </div>` +
+                (tracksHasError && !tracksHasWaiting ? `
+                <div class="col-xs-4 col-md-3">
+                    <div class="btn-group btn-group-justified" role="group" aria-label="Downloadalbum">
+                      <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-md btn-warning jsDownloadAnyway" data-url="${url}">Download anyway</button>
+                      </div>
+                      <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-md btn-info jsRetryAll" data-url="${url}">Retry all</button>
+                      </div>
+                    </div>
+                </div>` : '')
+                + '<div class="clearfix"></div>'
+            let launcher = `<div class="btn-group btn-group-justified" role="group" aria-label="Start download">
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-success jsCTAAccept" data-url="${url}">Download</button>
+                  </div>
+                  <div class="btn-group" role="group">
+                    <button type="button" class="btn btn-danger jsCTAReject" data-url="${url}">Reject</button>
+                  </div>
+                </div>`
+            let clickToAction = component.started ? progressBar : launcher
+            html += `<div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h2 class="panel-title">${component.artist} - ${component.album}</h2>
+                        </div>
+                        <div class="panel-body">
+                            ${clickToAction}
+                            <div class="col-sm-3">
+                                <img src="${component.content.albumart}" class="img-responsive img-rounded"/>
+                            </div>
+                            <div class="col-sm-9">
+                                <ol>
+                                    ${tracks}
+                                </ol>
+                            </div>
+                        </div>
+                    </div>`
+        }
+        if (html != '') {
+            document.getElementById('downloads_list').innerHTML = html
+        }
+
+        let pageHeader = `Downloads `
+
+        if (hasOneNotStarted) {
+            pageHeader += `<button type="button" class="btn btn-success jsAcceptAll">Download all</button>
+                        <button type="button" class="btn btn-danger jsRejectAll">Reject all</button>`
+
+        }
+        if (hasOneError && !hasOneWaiting) {
+            pageHeader += '<button type="button" class="btn btn-info jsRetryAllAlbums">Retry all</button>'
+        }
+        document.querySelector("#downloads .page-header h1").innerHTML = pageHeader
+        return this
+    }
+
+    bindDownloads() {
+        document.getElementById("downloads").addEventListener("click", function(e) {
+            if (e.target && e.target.matches(".jsCTAReject")) {
+                chrome.extension.getBackgroundPage().rejectDownload(e.target.attributes['data-url'].value)
+                window.location.reload()
+            }
+            if (e.target && e.target.matches(".jsCTAAccept")) {
+                chrome.extension.getBackgroundPage().startDownloadAlbum({url: e.target.attributes['data-url'].value})
+                window.location.reload()
+            }
+            if (e.target && e.target.matches(".jsDownloadAnyway")) {
+                chrome.extension.getBackgroundPage().downloadZip(e.target.attributes['data-url'].value)
+                window.location.reload()
+            }
+            if (e.target && e.target.matches(".jsRetryTrack")) {
+                chrome.extension.getBackgroundPage().downloadProcess(e.target.attributes['data-url'].value, e.target.attributes['data-trackid'].value)
+                window.location.reload()
+            }
+            if (e.target && e.target.matches(".jsRetryAll")) {
+                e.target.parentNode.parentNode.parentNode.parentNode.querySelectorAll('.jsRetryTrack').forEach(function (elm) {
+                    chrome.extension.getBackgroundPage().downloadProcess(elm.attributes['data-url'].value, elm.attributes['data-trackid'].value)
+                })
+                window.location.reload()
+            }
+            if (e.target && e.target.matches(".jsRetryAllAlbums")) {
+                document.getElementById('downloads_list').querySelectorAll('.jsRetryTrack').forEach(function (elm) {
+                   chrome.extension.getBackgroundPage().downloadProcess(elm.attributes['data-url'].value, elm.attributes['data-trackid'].value)
+                })
+               window.location.reload()
+            }
+            if (e.target && e.target.matches(".jsAcceptAll")) {
+                document.getElementById('downloads_list').querySelectorAll('.jsCTAAccept').forEach(function (elm) {
+                    chrome.extension.getBackgroundPage().startDownloadAlbum({url: elm.attributes['data-url'].value})
+                })
+                window.location.reload()
+            }
+            if (e.target && e.target.matches(".jsRejectAll")) {
+                document.getElementById('downloads_list').querySelectorAll('.jsCTAReject').forEach(function (elm) {
+                    chrome.extension.getBackgroundPage().rejectDownload(elm.attributes['data-url'].value)
+                })
+                window.location.reload()
+            }
+        });
+        window.addEventListener('storage', () => window.location.reload())
+        return this
+    }
+}
+
+window.addEventListener('load', () => new Settings(new Options()))
